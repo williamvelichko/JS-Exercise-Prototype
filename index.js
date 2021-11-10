efb5249c19d1d9c750a2e255766ce3293c1d48fa
williamvelichko <williamvelichko2003@gmail.com>
2021-11-10 12:11:00 -0800

    part of the first task is done

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ Airplane.prototype.land = function () {
   this.isFlying = false;
 };
 
-
 /*
 // 👇 COMPLETE YOUR WORK BELOW 👇
 // 👇 COMPLETE YOUR WORK BELOW 👇
@@ -39,15 +38,41 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
 
+Person.prototype.eat = function (edible) {
+  if (this.stomach.length < 10) {
+    this.stomach.push(edible);
+  }
+};
 
+Person.prototype.poop = function () {
+  this.stomach = [];
+};
 
+Person.prototype.toString = function () {
+  return `${this.name}, ${this.age}`;
+};
 
+const kyler = new Person("Kyler", 25);
+const ben = new Person("Ben", 20);
 
+console.log(kyler.toString());
+console.log(ben.toString());
+ben.eat("pizza");
+ben.eat("salad");
+ben.eat("icescream");
+ben.eat("cupcake");
+ben.eat("sushi");
 
+console.log(ben.stomach);
+
+console.log(ben.poop);
+console.log(ben.stomach);
 
 /*
   TASK 2
@@ -63,10 +88,7 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
-}
-
+function Car() {}
 
 /*
   TASK 3
@@ -75,10 +97,7 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
-}
-
+function Baby() {}
 
 /* 
   TASK 4
@@ -89,18 +108,17 @@ function Baby() {
   4. 
 */
 
-
 ///////// END OF CHALLENGE /////////
 
 /* 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 */
-function foo(){
-  console.log('its working!');
-  return 'bar';
+function foo() {
+  console.log("its working!");
+  return "bar";
 }
 foo();
 module.exports = {
   foo,
-  Person, 
+  Person,
   Car,
-  Baby
-}
+  Baby,
+};
